149ea17cfabeeff9f8032b0c6ac3eb7d9a3a1e0a
ktunprasert <ktunprasert@outlook.com>
2024-12-02 18:07:30 +0000

    main: run day 1

--- cmd/run/main.go
+++ cmd/run/main.go
@@ -1,7 +1,19 @@
 package main
 
-// import year_2024 "aoc/solver/2024"
+import (
+	"fmt"
+	"os"
+
+	year_2024 "aoc/solver/2024"
+)
 
 func main() {
-	// year_2024.Day1{}.part1("")
+	input, err := os.ReadFile("input/2024-1.txt")
+	if err != nil {
+		panic(err)
+	}
+
+	answer, _ := year_2024.Day1{}.Part1(string(input))
+	answer2, _ := year_2024.Day1{}.Part2(string(input))
+	fmt.Println(answer, answer2)
 }
